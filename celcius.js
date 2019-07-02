3fb5a75207a67595379517ba7afe38699d9ac8e1
imnabeel <imnabeel10@gmail.com>
2019-07-02 10:40:46 +0530

    first commit

---
+++ celcius.js
@@ -0,0 +1,10 @@
+var score = 100
+
+score = 130
+
+const bonous = 50
+
+var finalScore = score + bonous
+
+console.log (finalScore)
+
---
+++ variables.js
@@ -0,0 +1,14 @@
+let name = 'nabeel'
+
+let score = 100
+
+let bonous = 20
+
+let sum = score + bonous
+
+console.log(sum)
+
+let firstName = 'Leo'
+let lastName = 'Messi'
+
+console.log(firstName + ' ' + lastName)
